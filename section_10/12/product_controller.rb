2ded67622e0fbf0faa6db3f217b922457bc27a6b
haruto.watanabe <w.haru10n@gmail.com>
2019-01-09 10:03:27 +0900

    コンストラクタからファクトリメソッドへfirst

---
+++ section_10/12/product_controller.rb
@@ -0,0 +1,12 @@
+class ProductController
+  def create
+    @product = if imported
+                 ImportedProduct.new(base_price)
+                 if base_price > 1000
+                   LuxuryProduct.new(base_price)
+                 else
+                   Product.new(base_price)
+                 end
+               end
+  end
+end
